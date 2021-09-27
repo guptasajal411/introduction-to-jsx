8ce713d262e5ef9caee2e68d4cb4ac79afd93642
Sajal Gupta <guptasajal411@gmail.com>
2021-09-27 22:55:48 +0530

    Use curly braces for injecting JS expressions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var firstName = "Sajal";
+var lastName = "Gupta";
+var username = "guptasajal411";
+
 ReactDOM.render(
     <div>
-        <h1>Hello World!</h1>
+        <h1>Hello {firstName + " " + lastName}!</h1>
+        <p>Your username is {username}</p>
         <ul>
             <li>Unordered List with React</li>
             <li><code>ReactDOM.render</code> method can only take a single HTML element</li>
