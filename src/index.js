f244ba8eb6df0e5a42fde696e984f438408411d8
Sajal Gupta <guptasajal411@gmail.com>
2021-09-27 22:48:23 +0530

    Add list in element

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import ReactDOM from 'react-dom';
 ReactDOM.render(
     <div>
         <h1>Hello World!</h1>
-        <p>My first react app!</p>
+        <ul>
+            <li>Unordered List with React</li>
+            <li><code>ReactDOM.render</code> method can only take a single HTML element</li>
+            <li>Babel compiles JS code so that every browser can understand it</li>
+        </ul>
     </div>,
     document.querySelector("#root")
 );
