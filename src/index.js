e24f9727c381d136ee99e57f77838714747dd324
Sajal Gupta <guptasajal411@gmail.com>
2021-09-28 11:35:33 +0530

    Add inline styles with props

--- src/index.js
+++ src/index.js
@@ -5,9 +5,19 @@ var firstName = "Sajal";
 var lastName = "Gupta";
 var picsum = "http://picsum.photos/"
 
+const customStyle = {
+    color: "white",
+    fontSize: "1 rem",
+    backgroundColor: "black",
+    border: "1px solid red"
+}
+
+customStyle.fontSize = "1.5rem"
+
 ReactDOM.render(
     <div>
         <h1 className="heading" contentEditable="true" spellCheck="false">My Tech Stack</h1>
+        <p style={customStyle}>My name is {firstName + ' ' + lastName}</p>
         <div>
             <img className="tech-image" src={picsum + 200 + '?grayscale'} />
         </div>
