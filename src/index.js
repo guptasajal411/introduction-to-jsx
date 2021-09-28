f80eb97a5b18c120869ee727fcff4106f3ddd2e7
Sajal Gupta <guptasajal411@gmail.com>
2021-09-28 11:46:02 +0530

    Create dynamic styles with Date() in JSX

--- src/index.js
+++ src/index.js
@@ -4,20 +4,47 @@ import ReactDOM from 'react-dom';
 var firstName = "Sajal";
 var lastName = "Gupta";
 var picsum = "http://picsum.photos/"
+var date = new Date();
 
-const customStyle = {
-    color: "white",
-    fontSize: "1 rem",
-    backgroundColor: "black",
-    border: "1px solid red"
+// const customStyle = {
+//     color: "white",
+//     fontSize: "1 rem",
+//     backgroundColor: "black",
+//     border: "1px solid red"
+// }
+
+// customStyle.fontSize = "1.5rem"
+
+var time = date.getHours();
+var welcomeMessage;
+var customStyle;
+if (time > 6 && time < 12){
+    welcomeMessage = "Good Morning";
+    customStyle = {
+        color: "red"
+    }
+} else if (time > 12 && time < 18){
+    welcomeMessage = "Good Afternoon"
+    customStyle = {
+        color: "green"
+    }
+} else if (time > 18 && time < 24){
+    welcomeMessage = "Good Evening"
+    customStyle = {
+        color: "orange"
+    }
+} else {
+    welcomeMessage = "Good Night"
+    customStyle = {
+        color: "blue"
+    }
 }
 
-customStyle.fontSize = "1.5rem"
 
 ReactDOM.render(
     <div>
-        <h1 className="heading" contentEditable="true" spellCheck="false">My Tech Stack</h1>
-        <p style={customStyle}>My name is {firstName + ' ' + lastName}</p>
+        <h4 style={customStyle} className="heading">{welcomeMessage}, The time is {time}</h4>
+        <p>My name is {firstName + ' ' + lastName}</p>
         <div>
             <img className="tech-image" src={picsum + 200 + '?grayscale'} />
         </div>
